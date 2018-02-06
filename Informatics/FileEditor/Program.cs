8cde899a6e197dbf2b7f3a0e28baca8ecdd3f9cc
HristoTraykov18 <33829877+HristoTraykov18@users.noreply.github.com>
2018-02-06 13:24:54 +0200

    Added exercises from TU

---
+++ Informatics/FileEditor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FileEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileBytes = new BytesList();
+            try
+            {
+                using (var f = File.Open("storage.txt", FileMode.Open))
+                {
+                    int read;
+                    do
+                    {
+                        var buffer = new byte[10];
+                        read = f.Read(buffer, 0, buffer.Length);
+                        fileBytes.AddRange(buffer, read);
+                    }
+                    while (read > 0);
+                    var s = Encoding.UTF8.GetString(fileBytes.ToArray());
+                    Console.Write(s);
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File not found: " + e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
